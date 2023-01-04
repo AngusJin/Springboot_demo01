3c5c7e830ed0766b6c809904d87a2c1f695afca0
Angus <hdongpro@outlook.com>
2023-01-04 15:42:51 +0800

    Initial commit

---
+++ demo01/src/main/java/com/demo01/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo01.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String hello(){
+        return "111";
+    }
+
+}
